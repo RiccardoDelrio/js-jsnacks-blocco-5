369a9280de5f01478511a708d549fb8b0379b7de
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-26 15:37:26 +0100

    create a new array with map. contains only the author's name

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const autori = posts.map((element) => {
+  thisAutor = element.author
+  return thisAutor
+
+})
+console.log(autori);
+
